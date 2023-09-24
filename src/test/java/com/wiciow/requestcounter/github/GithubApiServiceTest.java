fd22a689526db17be945d73dba6fb185acba6bac
wiktor <wiktorwolakksiazekjob@gmail.com>
2023-09-24 13:48:28 +0200

    clean up

--- src/test/java/com/wiciow/requestcounter/github/GithubApiServiceTest.java
+++ src/test/java/com/wiciow/requestcounter/github/GithubApiServiceTest.java
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.web.client.RestClientException;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
